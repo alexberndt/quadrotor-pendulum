0f48746ac22c11a4bb6ff1c0c8228a1ab7a34bbe
Matthijs <matthijsbekendam@gmail.com>
2019-02-18 13:28:39 +0100

    Simulatie quad matthijs

--- matthijs.m
+++ matthijs.m
@@ -8,8 +8,8 @@
 % Constants
 g = 9.81;   % m/s^2
 L = 0.6;    % meters
-
-A = [0   1 0 0 0;
+%123
+A = [0 1 0 0 0;
      g/L 0 0 0 -g;
      0   0 0 1 0;
      0   0 0 0 g;
@@ -41,6 +41,20 @@ end
 % we have three uncontrollable modes lambda = 0,0,0
 
 Q = eye(size(A));
-R = 1;
+R = 0.1;
+Ts = 0.1
+C = eye(size(A));
+SYSC = ss(A,B,C,0)
+SYSD = c2d(SYSC,Ts,'zoh')
+Ad = SYSD.A 
+Bd = SYSD.B 
+[K,S,e] = dlqr(Ad,Bd,Q,R,[]);
+iterations = 1000;
+x = zeros(5,length(iterations))
+x0 = [1 1 0 0 1]';
+x(:,1) = x0
+for k = 1:iterations
+    x(:,k+1) = Ad*x(:,k)-Bd*K*x(:,k);
+end
 
-[K,S,e] = lqr(A,B,Q,R,[]);
+stairs(linspace(1,iterations+1,iterations+1)*Ts,x(1,:))
