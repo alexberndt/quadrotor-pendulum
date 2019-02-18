21a2528f8e72e1dae5ae3b3f49394260300f1421
lx <berndtae@gmail.com>
2019-02-18 14:53:37 +0100

    Changed to discrete-time model and simulation

--- quadrotor_pendulum.m
+++ quadrotor_pendulum.m
@@ -9,49 +9,16 @@
 g = 9.81;   % m/s^2
 L = 0.6;    % meters
 
-A = [0   1 0 0 0;
+% continuous-time state space matrices
+Ac = [0   1 0 0 0;
      g/L 0 0 0 -g;
      0   0 0 1 0;
      0   0 0 0 g;
      0   0 0 0 0];
  
-B = [0;0;0;0;1];
- 
-%% CHECK CONTROLLABILITY OF CONTINUOUS TIME SYSTEM
- 
-ctrb(A,B)
+Bc = [0;0;0;0;1];
 
-% not controllable
-% use Hautus test 
-
-eigvals = eig(A);
-
-for idx = 1:numel(eigvals)
- lambda = eigvals(idx);
- disp('Eigenvalue: ');
- disp(lambda)
- rk = rank([(eye(size(A))*lambda-A) B]);
- disp('rank: ');
- disp(rk);
- disp('----------------');
-end
-
-%% DISCRETIZE SYSTEM
-
-
-
-%% LINEAR QUADRATIC REGULATOR
-
-% we have three uncontrollable modes lambda = 0,0,0
-
-Q = eye(size(A));
-R = 0.1;
-
-[K,S,e] = lqr(A,B,Q,R,[]);
-
-% SIMUALTION
-
-C = eye(size(A));
+Cc = eye(size(Ac));
 % outputs we are interested are
 % - r1:     the displacement of 
 % - r2:     velocity of pendulum relative to quadrotor
@@ -61,9 +28,107 @@ C = eye(size(A));
 %           coordinate frame O
 % - beta:   pitch angle of quad (rotation angle around y-axis)
 
-sys = ss(A-B*K,B,C,[]);
+% continuous-time state-space model
+sysc = ss(Ac,Bc,Cc,[]);
+ 
+%% CHECK CONTROLLABILITY OF CONTINUOUS TIME SYSTEM
+ 
+% ctrb(A,B)
+% 
+% % not controllable
+% % use Hautus test 
+% 
+% eigvals = eig(A);
+% 
+% for idx = 1:numel(eigvals)
+%  lambda = eigvals(idx);
+%  disp('Eigenvalue: ');
+%  disp(lambda)
+%  rk = rank([(eye(size(A))*lambda-A) B]);
+%  disp('rank: ');
+%  disp(rk);
+%  disp('----------------');
+% end
+
+%% DISCRETIZE SYSTEM
+
+Ts = 0.1;  % 10 ms sampling time
+sysd = c2d(sysc,Ts);
+
+A = sysd.A;
+B = sysd.B;
+C = sysd.C;
+
+%% LINEAR QUADRATIC REGULATOR
+
+% we have three uncontrollable modes lambda = 0,0,0
+
+Q = eye(size(A));
+R = 0.1;
+
+[K,S,e] = dlqr(A,B,Q,R,[]);
+
+%% SIMUALTION
+
+sysd_closedloop = ss(A-B*K,B,C,[],Ts);
+
+[states_trajectory, time] = step(sysd_closedloop);
+
+%% PLOT SIMULATION RESULTS
 
 figure(1);
 clf;
-step(sys);
+stairs(time, states_trajectory(:,1));
 grid();
+
+%% PLOT SIMULATION GRAPHICALLY
+show_trajectory = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% .
