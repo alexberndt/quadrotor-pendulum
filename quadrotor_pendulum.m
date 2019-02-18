1ce21c839c9217279ee99c943465e1e09ea12d9e
lx <berndtae@gmail.com>
2019-02-18 11:58:09 +0100

    Improved script readability

--- quadrotor_pendulum.m
+++ quadrotor_pendulum.m
@@ -1,10 +1,13 @@
-%% Basic script for quadrotor with pendulum
+%% QUADROTOR BALANCING PENDULUM MODEL PREDICTIVE CONTROL SIMULATION
+%
+% MATLAB simulation of the paper A Flying Inverted Pendulum by Markus Hehn 
+% and Raffaello D'Andrea 
+
+%% INITIALIZATION 
 
 % Constants
 g = 9.81;   % m/s^2
 L = 0.6;    % meters
-
-% y rotation
 
 A = [0   1 0 0 0;
      g/L 0 0 0 -g;
@@ -12,31 +15,32 @@ A = [0   1 0 0 0;
      0   0 0 0 g;
      0   0 0 0 0];
  
- B = [0;0;0;0;1];
+B = [0;0;0;0;1];
  
-%  A = [0 1 0; 0 0 1; 0 0 0];
-%  B = [0;0;1];
+%% CHECK CONTROLLABILITY
  
- ctrb(A,B)
- 
- % not controllable
- % use Hautus test 
- 
- eigvals = eig(A);
- 
- for idx = 1:numel(eigvals)
-     lambda = eigvals(idx);
-     disp('Eigenvalue: ');
-     disp(lambda)
-     rk = rank([(eye(size(A))*lambda-A) B]);
-     disp('rank: ');
-     disp(rk);
-     disp('----------------');
- end
- 
- % we have three uncontrollable modes lambda = 0,0,0
- 
- Q = eye(size(A));
- R = 1;
- 
- [K,S,e] = lqr(A,B,Q,R,[]);
+ctrb(A,B)
+
+% not controllable
+% use Hautus test 
+
+eigvals = eig(A);
+
+for idx = 1:numel(eigvals)
+ lambda = eigvals(idx);
+ disp('Eigenvalue: ');
+ disp(lambda)
+ rk = rank([(eye(size(A))*lambda-A) B]);
+ disp('rank: ');
+ disp(rk);
+ disp('----------------');
+end
+
+%% LINEAR QUADRATIC REGULATOR
+
+% we have three uncontrollable modes lambda = 0,0,0
+
+Q = eye(size(A));
+R = 1;
+
+[K,S,e] = lqr(A,B,Q,R,[]);
